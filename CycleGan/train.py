8d50cba2ee1fbf98940a8b8d06668eb845c45d03
Kemal Utku Lekesiz <kemalutkulekesiz@gmail.com>
2021-08-10 22:39:19 +0300

    Changed batch normalization layers to instance normalization

--- CycleGan/train.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-class GeneratorA(tf.keras.Model):
-    def __init__(self, feature_map_size, n_channels):
-
-class GeneratorB(tf.keras.Model):
-    pass
--- dcGan/configuration.py
+++ dcGan/configuration.py
@@ -1,14 +1,14 @@
 dataset_dir = r"C:\Users\Utku\Documents\Dataset\imagesCrop2.record"
-batch_size = 8
+batch_size = 16
 image_size = 64
-latent_vector_size = 100
+latent_vector_size = 200
 generator_feature_map_count = 128
-discriminator_feature_map_count = 16
+discriminator_feature_map_count = 32
 num_epochs = 50
 learning_rate = 0.0002
 beta1 = 0.5
 gpu = '0'
-gen_disc_train_ratio = 1
+gen_disc_train_ratio = 2
 
 info = ""
 
